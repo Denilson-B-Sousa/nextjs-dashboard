f33a76fad6de5df3cba0c2d65af87c8a539bd26a
Denilson <denilsob.b.sousa2021@gmail.com>
2024-11-22 11:41:29 -0300

    feat: add learn nextjs code: chapter one to five

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+
+/**The layout file is the best way to create a shared layout that all pages in your application can use. */
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body className={` ${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
